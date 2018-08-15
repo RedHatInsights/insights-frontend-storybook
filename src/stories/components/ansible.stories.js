52d735b5e5b9193c53c1f5b55e6146cb863656d7
Ryan Long <rlong@redhat.com>
2018-08-15 11:13:38 -0400

    adding battery and ansible icon

---
+++ src/stories/components/ansible.stories.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+// Libraries
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import chaptersAddon from 'react-storybook-addon-chapters';
+import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
+
+// Styling
+import '../../stories.scss';
+
+// Component
+import { Ansible } from '@red-hat-insights/insights-frontend-components';
+
+// README
+import PageHeaderReadme from '../../docs/components/ansible/README.md';
+import { withReadme }  from 'storybook-readme';
+
+storiesOf('Components/Ansible Icon', module)
+    .addDecorator(withReadme(PageHeaderReadme))
+    .addDecorator(withKnobs)
+    .addWithChapters(
+        'Overview',
+        {
+            subtitle: 'Ansible styling overview',
+            chapters: [
+                {
+                    title: 'Demo',
+                    subtitle: 'Use the "Knobs" tab in the addon panel to use interactive preview',
+                    sections: [
+                        {
+                            title: 'Interactive Preview',
+                            sectionFn: ('', () => {
+                                return(
+                                    <Ansible unsupported={boolean('unsupported', false)}/>
+                                )
+                            })
+                        },
+                    ],
+                }
+            ]
+        }
+    )
