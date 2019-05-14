a15129148f707c73247a2f6b8685bdf2ad55b6cf
Ryan Long <rlong@redhat.com>
2019-05-14 09:13:05 -0400

    adding custom release

---
+++ .travis/custom_release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+
+if [ "${TRAVIS_BRANCH}" = "master" ]
+then
+    for env in ci qa prod
+    do
+        echo "PUSHING ${env}-beta"
+        rm -rf ./dist/.git
+        .travis/release.sh "${env}-beta"
+    done
+fi
+
+
+if [ "${TRAVIS_BRANCH}" = "master-stable" ]
+then
+    for env in ci qa prod
+    do
+        echo "PUSHING ${env}-stable"
+        rm -rf ./dist/.git
+        .travis/release.sh "${env}-stable"
+    done
+fi
