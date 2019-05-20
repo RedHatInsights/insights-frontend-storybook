9399ffc7efe16004a77740a69bf9e318b2b531e4
Ryan Long <rlong@redhat.com>
2019-05-20 14:51:56 -0400

    updating the welcome/quick start stuff with Yash findings

--- src/stories/welcome/welcome.stories.js
+++ src/stories/welcome/welcome.stories.js
@@ -7,10 +7,12 @@ import WelcomeDoc from '../../docs/welcome/gettingStarted/DOC.md';
 import WelcomeReadme from '../../docs/welcome/gettingStarted/README.md';
 import QuickStartDoc from '../../docs/welcome/quickStart/DOC.md';
 import ContributingDoc from '../../docs/welcome/contributing/DOC.md';
+import EnvironmentsDoc from '../../docs/welcome/environments/DOC.md';
 
 // Story
 storiesOf('Welcome', module)
     .addDecorator(withReadme(WelcomeReadme))
     .add('Getting Started', doc(WelcomeDoc))
     .add('Quick Start Guide', doc(QuickStartDoc))
+    .add('Environments', doc(EnvironmentsDoc))
     .add('Contributing', doc(ContributingDoc))
