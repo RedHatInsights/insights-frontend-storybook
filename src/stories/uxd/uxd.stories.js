d77149f299c3df2e9c714937db576df6c94c6d00
Ryan Long <rlong@redhat.com>
2019-06-05 14:18:43 -0400

    Update uxd.stories.js

--- src/stories/uxd/uxd.stories.js
+++ src/stories/uxd/uxd.stories.js
@@ -12,7 +12,7 @@ import LanguageDoc from '../../docs/uxd/language/DOC.md';
 import ToolbarsDoc from '../../docs/uxd/toolbarsFilters/DOC.md';
 import EmptyStatesDoc from '../../docs/uxd/emptyStates/DOC.md';
 import GetStarted from '../../docs/uxd/getStarted/DOC.md';
-import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md'
+import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md';
 
 // This part loads in everything that is actually going to be there. All these are the items under the menu
 // Reference anything you add up there ^ to down there v
