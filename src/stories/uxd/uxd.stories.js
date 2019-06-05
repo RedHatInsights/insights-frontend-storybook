2134a13bcdac1fe3e23c8246e1278b5af4a5dbfa
Becca Glass <bglass@redhat.com>
2019-06-05 12:02:16 -0400

    fixing bugs in uxd.stories.js

--- src/stories/uxd/uxd.stories.js
+++ src/stories/uxd/uxd.stories.js
@@ -12,7 +12,7 @@ import LanguageDoc from '../../docs/uxd/language/DOC.md';
 import ToolbarsDoc from '../../docs/uxd/toolbarsFilters/DOC.md';
 import EmptyStatesDoc from '../../docs/uxd/emptyStates/DOC.md';
 import GetStarted from '../../docs/uxd/getStarted/DOC.md';
-import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md'
+import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md';
 
 // This part loads in everything that is actually going to be there. All these are the items under the menu
 // Reference anything you add up there ^ to down there v
@@ -20,7 +20,7 @@ import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md'
 storiesOf('UX', module)
     .addDecorator(withReadme(UXDReadme))
     .add('Get started', doc(GetStarted))
-    .add('Actions on systems', doc([ActionsOnSystemsDoc]))
+    .add('Actions on systems', doc(ActionsOnSystemsDoc))
     .add('Alerts & notifications', doc(AlertsNotificationsDoc))
     .add('Empty states', doc(EmptyStatesDoc))
     .add('Language & terminology', doc(LanguageDoc))
