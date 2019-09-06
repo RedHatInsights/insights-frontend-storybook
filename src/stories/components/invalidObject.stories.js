0f2f912b9d984fff64727ff6f469918143f52424
Ryan Dillin Felton <ryandillinfelton@gmail.com>
2019-09-06 11:29:49 -0400

    invalidObject docs (#80)
    * invalidObject docs
    
    * invalidObject docs
    
    * added story for InvalidObject component
    
    * removed subtitle

---
+++ src/stories/components/invalidObject.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// Libraries
+import { storiesOf } from '@storybook/react';
+import { withKnobs } from '@storybook/addon-knobs';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+// Component
+import { InvalidObject } from '@redhat-cloud-services/frontend-components';
+
+// README
+import InvalidObjectReadMe from '../../docs/components/invalidObject/README.md';
+import { withReadme }  from 'storybook-readme';
+
+// Story
+storiesOf('Components', module)
+    .addDecorator(withReadme(InvalidObjectReadMe))
+    .addDecorator(withKnobs)
+    .addWithChapters(
+        'Invalid Object',
+        {
+            chapters: [
+                {
+                    title: 'Demo',
+                    sections: [
+                        {
+                            title: 'Interactive Preview',
+                            sectionFn: ('', () => {
+                                return(
+                                    <Router>
+                                        <InvalidObject />
+                                    </Router>
+                                )
+                            })
+                        },
+                    ],
+                }
+            ]
+        }
+    )
