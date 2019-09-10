7101cbc3ce317c3e1fda5ae68d3ea7c286eee5ff
katierik <kriker@redhat.com>
2019-09-10 12:54:36 -0400

    testing in workshop (#61)

--- src/stories/uxd/uxd.stories.js
+++ src/stories/uxd/uxd.stories.js
@@ -12,7 +12,8 @@ import LanguageDoc from '../../docs/uxd/language/DOC.md';
 import ToolbarsDoc from '../../docs/uxd/toolbarsFilters/DOC.md';
 import EmptyStatesDoc from '../../docs/uxd/emptyStates/DOC.md';
 import GetStarted from '../../docs/uxd/getStarted/DOC.md';
-import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md'
+import ActionsOnSystemsDoc from '../../docs/uxd/actionsOnSystems/DOC.md';
+import Breadcrumbs from '../../docs/uxd/breadcrumbs/DOC.md';
 
 // This part loads in everything that is actually going to be there. All these are the items under the menu
 // Reference anything you add up there ^ to down there v
@@ -22,6 +23,7 @@ storiesOf('UX', module)
     .add('Get started', doc(GetStarted))
     .add('Actions on systems', doc(ActionsOnSystemsDoc))
     .add('Alerts & notifications', doc(AlertsNotificationsDoc))
+    .add('Breadcrumbs', doc(Breadcrumbs))
     .add('Empty states', doc(EmptyStatesDoc))
     .add('Language & terminology', doc(LanguageDoc))
     .add('Loading states', doc(LoadingDoc))
